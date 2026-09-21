7689b7dbe31c4d57ad72e7da2da9beae5085721f
Sunniva Sørensen <sunnsor@gmail.com>
2026-09-21 13:44:29 +0200

    Updated to use axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { Provider } from "@/components/ui/provider"
 import React from "react"
-import ReactDOM from "react-dom/client"
 import App from "./App"
+import ReactDOM from "react-dom/client"
 import "./index.css"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <Provider>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </QueryClientProvider>
 )
